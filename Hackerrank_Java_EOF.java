757f01cc76bf7d28e691ce9171f2be5ca3b3568d
gauta25 <gautamkanwar9@gmail.com>
2025-08-22 11:37:18 -0700

    Create Hackerrank_EOF.java

---
+++ Hackerrank_Java_EOF.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            System.out.println(index, input_num)
+        }
+    }
+}
